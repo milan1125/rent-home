98c378406c89ac8ef65574034c8d6a3b9dc69f50
milan1125 <milansavaliya1125@gmail.com>
2024-09-03 00:25:21 +0530

    auth uri change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-1zr1ogdbpdovgzzw.us.auth0.com"
      clientId="k04iq8vUDElXge4botZeQ5W9JWALmfCL"
      authorizationParams={{
-      redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.app"
+      redirect_uri: "https://rent-home-flax.vercel.app"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
