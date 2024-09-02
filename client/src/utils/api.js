53111ec44d587b2598c753ce23508e8356dc05da
milan1125 <milansavaliya1125@gmail.com>
2024-09-03 00:12:10 +0530

    baseurl change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate-youtube.vercel.app/api",
+  baseURL: "https://rent-home-sigma.vercel.app/",
 });
 
 export const getAllProperties = async () => {
